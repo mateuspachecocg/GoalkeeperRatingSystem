1b8d0987d52b036f9ca4430f641a844933d6e4d8
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-10-14 09:52:29 -0300

    Creating the Persistence and methods to read the Files

--- src/Control/Main.java
+++ src/Control/Main.java
@@ -1,20 +1,81 @@
 package Control;
 
-import java.util.ArrayList;
-
-import Model.Goalkeeper;
+import java.util.*;
+import Model.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
 	public static void main(String[] args) {
+		ArrayList<Shot> bd_shots = loadShots();
+		ArrayList<Goalkeeper> bd_goalkeepers = loadGoalkeepers();
+		
+	}
 
+	public static ArrayList<Shot> loadShots() {
+		int amountShot, shotId, shotStrength, shotQuadrant, shotPx, shotPy;
+		ArrayList<Shot> shots = new ArrayList<Shot>();
+		File fileObject;
+		Scanner fileReader;
+
+		try {
+			fileObject = new File("bd_shots.txt");
+			fileReader = new Scanner(fileObject);
+			amountShot = fileReader.nextInt();
+
+			for (int i = 0; i < amountShot; i++) {
+				shotId = fileReader.nextInt();
+				shotStrength = fileReader.nextInt();
+				shotQuadrant = fileReader.nextInt();
+				shotPx = fileReader.nextInt();
+				shotPy = fileReader.nextInt();
+				shots.add(new Shot(shotId, shotStrength, shotQuadrant, shotPx, shotPy));
+			}
+
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return shots;
 	}
 
 	public static ArrayList<Goalkeeper> loadGoalkeepers() {
+		int amountGoalpeekers, goalpeekerId, goalkeeperSpeed, goalkeeperFlexibility, goalkeeperAgility,
+				goalkeeperCoordination, goalkeeperStrength, goalkeeperBalance;
+		String goalkeeperName;
 		ArrayList<Goalkeeper> goalkeepers = new ArrayList<Goalkeeper>();
+		File fileObject;
+		Scanner fileReader;
+
+		try {
+			fileObject = new File("bd_goalpeekers.txt");
+			fileReader = new Scanner(fileObject);
+			amountGoalpeekers = fileReader.nextInt();
+
+			for (int i = 0; i < amountGoalpeekers; i++) {
+				goalpeekerId = fileReader.nextInt();
+				goalkeeperName = fileReader.nextLine();
+				goalkeeperSpeed = fileReader.nextInt();
+				goalkeeperFlexibility = fileReader.nextInt();
+				goalkeeperAgility = fileReader.nextInt();
+				goalkeeperCoordination = fileReader.nextInt();
+				goalkeeperStrength = fileReader.nextInt();
+				goalkeeperBalance = fileReader.nextInt();
+				goalkeepers.add(new Goalkeeper(goalpeekerId, goalkeeperName, goalkeeperSpeed, goalkeeperFlexibility,
+						goalkeeperAgility, goalkeeperCoordination, goalkeeperStrength, goalkeeperBalance));
+			}
+
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
 		goalkeepers.add(new Goalkeeper(1, "Patrick Skaggs", 5, 7, 8, 9, 6, 6));
 		goalkeepers.add(new Goalkeeper(2, "Uehudal Hack", 9, 6, 8, 8, 7, 10));
-		return null;
+		return goalkeepers;
 
 	}
 
